a3fcd0ea874dc09d0cfbc4d9fc4775e9b721c3be
Keyur Gajjar <kga@odoo.com>
2018-08-20 18:41:10 +0530

    [IMP] dynamic quantity update on pickings.

--- farmproject_inventory/models/stock_move.py
+++ farmproject_inventory/models/stock_move.py
@@ -2,19 +2,30 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import api, fields, models
 from odoo.tools.float_utils import float_is_zero, float_compare
+from odoo.addons import decimal_precision as dp
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     purchase_uom = fields.Many2one(comodel_name="product.uom", compute='_compute_product_uom', string="Unit of Measure")
-    product_uom_po_qty = fields.Float(string="Initial Demand")
-    po_qty_done = fields.Float(compute="_quantity_done_compute", string="Done")
+    product_uom_po_qty = fields.Float(string="Initial Demand", compute="_compute_product_uom_po_qty", digits=dp.get_precision('Product Unit of Measure'), inverse="_product_uom_qty_set")
+    po_qty_done = fields.Float(compute="_quantity_done_compute", string="Done", inverse="_po_qty_done_set", digits=dp.get_precision('Product Unit of Measure'))
 
     @api.depends('picking_code')
     def _compute_product_uom(self):
         for move in self:
             move.purchase_uom = move.product_id.uom_po_id if move.picking_code == 'incoming' else move.product_id.uom_id
+
+    @api.multi
+    def _compute_product_uom_po_qty(self):
+        for move in self:
+            move.product_uom_po_qty = move.product_uom._compute_quantity(move.product_uom_qty, move.product_id.uom_po_id, rounding_method='HALF-UP') if move.picking_code == 'incoming' else move.product_uom_qty
+
+    def _product_uom_qty_set(self):
+        for move in self:
+            if move.product_uom_po_qty:
+                move.product_uom_qty = move.purchase_uom._compute_quantity(move.product_uom_po_qty, move.product_uom, rounding_method='HALF-UP')
 
     def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
         vals = super(StockMove, self)._prepare_move_line_vals(quantity=quantity, reserved_quant=reserved_quant)
@@ -45,3 +56,7 @@ class StockMove(models.Model):
                 qty_po_done += move_line.product_uom_id._compute_quantity(move_line.qty_done, move.purchase_uom, round=False)
             move.quantity_done = quantity_done
             move.po_qty_done = qty_po_done
+
+    def _po_qty_done_set(self):
+        self[0].quantity_done = self[0].po_qty_done
+        self._quantity_done_set()
